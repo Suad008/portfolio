c6bccf9cdcaae45c9600baa13e2595f29d80e824
Suad008 <asadullayevsuad679@gmail.com>
2025-05-06 17:54:32 +0400

    changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return (
+    <Router className="App">
+      <NavBar />
+    </Router>
+  );
+};
+
+export default App;
